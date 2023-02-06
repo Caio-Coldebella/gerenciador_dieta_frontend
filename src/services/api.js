118071301122428e8ddfd7f3f9c0d402cf284a17
Caio Ruiz Coldebella <63621516+Caio-Coldebella@users.noreply.github.com>
2023-02-06 18:53:34 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 //POSTGRES_HOST=localhost
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost/api'
 });
 
 export default instance;
